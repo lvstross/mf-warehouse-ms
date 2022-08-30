80c2f0d3f326abd8f93d141253362dfc3407c6a2
Levi Gonzales <lvstross@gmail.com>
2022-08-30 16:31:45 -0600

    Added tailwind headless react and header

--- wms-host/src/App.tsx
+++ wms-host/src/App.tsx
@@ -1,18 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Customers from "customers/Customers";
-import Departments from "departments/Departments";
+import Header from './components/Header';
 
 import "./index.scss";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Customers />
-    <Departments />
-    <div>Name: wms-host</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+  <div>
+    <Header />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
